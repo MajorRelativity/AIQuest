b993c32c1013eb03d1a96c9de213040505c15642
mrmorel15 <148816764+mrmorel15@users.noreply.github.com>
2024-09-28 15:08:23 -0400

    Choose Motive Function

--- AIQuest.py
+++ AIQuest.py
@@ -12,9 +12,14 @@ def choose_random_traits():
     trait_list = ["Kind", "Arrogant", "Friendly", "Smart", "Confident", "Loyal", "Strong", "Mad", "Sad", "Decisive"]
     print(random.choice(trait_list))
 
+def choose_motive():
+    motive_list = ["I loved them but they didn't love me back", "I wanted their land", "They were blackmailing me", "They saw me stealing bread", "They dated the person I love", "I got paid to do it", "They creeped me out", "They wouldn't stop bothering me for the debt I owed", "I wanted to be noticed", "I discovered they caused the accident that killed my father"]
+    print(random.choice(motive_list))
+
 def main():
     choose_random_name()
     choose_random_job()
     choose_random_traits()
+    choose_motive()
 
 main()
