eccde526ac00d5f0763f6281c1a320be8162d56f
mrmorel15 <148816764+mrmorel15@users.noreply.github.com>
2024-09-28 14:41:04 -0400

    Random Job Function

--- AIQuest.py
+++ AIQuest.py
@@ -4,7 +4,12 @@ def choose_random_name():
     name_list = ["Gavin", "Alyssa", "Mary", "Ebenezer", "Bridgette", "Tobias", "Leona", "Franklin", "Annie", "Blake", "Beth", "Barnaby", "Brie", "Elijah", "Aubrey"]
     print(random.choice(name_list))
     
+def choose_random_job():
+    job_list = ["Librarian", "Farmer", "Fisherman", "Banker", "Artist", "Mayor", "Doctor", "Maid", "Miner", "Hunter", "Barber", "Carpenter", "Mailman", "Baker", "Trader"]
+    print(random.choice(job_list))
+
 def main():
     choose_random_name()
+    choose_random_job()
 
 main()
