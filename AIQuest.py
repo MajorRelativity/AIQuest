bf95a83fdda6cba4b2790cbf763410e4420eee8f
MajorRelativity <80081144+MajorRelativity@users.noreply.github.com>
2024-09-28 15:47:08 -0400

    First GPT API Call

--- AIQuest.py
+++ AIQuest.py
@@ -1,4 +1,24 @@
+from openai import OpenAI
+
+f = open("API_KEY.txt","r")
+API_KEY = f.read()
+f.close()
+
+client = OpenAI(api_key = API_KEY)
+
+def initialize_chatGPT():
+
+    response = client.chat.completions.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {"role": "user", "content": "What is your favorite color?"}
+        ]
+    )
+
+    print(response)
+
 def main():
+    initialize_chatGPT()
     print("Hello World")
-    
+
 main()
