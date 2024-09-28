c4d11630bdb6ed21e00dda3587d8057359cf6f57
mrmorel15 <148816764+mrmorel15@users.noreply.github.com>
2024-09-28 14:24:37 -0400

    Random Name Function

--- AIQuest.py
+++ AIQuest.py
@@ -1,5 +1,8 @@
+import random
+
 def choose_random_name():
-    print("Bob")
+    name_list = ["Gavin", "Alyssa", "Mary", "Ebenezer", "Bridgette", "Tobias", "Leona", "Franklin", "Annie", "Blake", "Beth", "Barnaby", "Brie", "Elijah", "Aubrey"]
+    print(random.choice(name_list))
     
 def main():
     choose_random_name()
