8ae965f5b722ec06a608485532359119fab07b08
MajorRelativity <80081144+MajorRelativity@users.noreply.github.com>
2024-09-28 16:40:23 -0400

    Improved User Prompt

--- AIQuest.py
+++ AIQuest.py
@@ -40,24 +40,30 @@ def prompt_gpt(gpt_role: str, user_question: str):
     response = gpt_steam.choices[0].message.content
     return response
 
-def get_user_question(name: str):
+def get_user_question(name: str, user_prompt: str, suspect_number: int):
     ''' 
      Prompts the user for what question they want to ask the suspect.
      Takes:
         - name (str): Name of the suspect they are questioning
-     
+        - user_promt (str): What the suspect says to the user
+        - suspect_numer (int): What order in the interview process the suspect is 
      Returns:
         - user_question (str): The question the user wants to ask the suspect
     
     '''
+    if suspect_number == 1:
+        print("You approach the first subject")
+    else:
+        print("You approach the next subject")
 
+    print(f"[{name}]: {user_prompt}")
     user_question = input(f"What question would you like to ask {name}? ")
     return user_question
 
 
 
 def main():
-     user_question = get_user_question("Jacob")
+     user_question = get_user_question("Jacob","Hi I am Jacob",1)
      print(prompt_gpt("Your favorite color is blue", user_question))
 
 main()
