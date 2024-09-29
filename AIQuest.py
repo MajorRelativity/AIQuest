439c50c2d513b47f33c855ade1032681b8e36669
MajorRelativity <80081144+MajorRelativity@users.noreply.github.com>
2024-09-29 10:27:23 -0400

    Update Names
    - We now have a more exhaustive list of unique names to choose from

--- AIQuest.py
+++ AIQuest.py
@@ -225,7 +225,14 @@ def get_user_question(name: str, user_prompt: str, suspect_number: int):
     return user_question
 
 def choose_random_name():
-    name_list = ["Gavin", "Alyssa", "Mary", "Ebenezer", "Bridgette", "Tobias", "Leona", "Franklin", "Annie", "Blake", "Beth", "Barnaby", "Brie", "Elijah", "Aubrey"]
+    f = open("first_names.txt","r")
+    name_list = []
+    name = f.readline().strip()
+    while name != '':
+        name_list.append(name)
+        name = f.readline().strip()
+    f.close()
+    #name_list = ["Gavin", "Alyssa", "Mary", "Ebenezer", "Bridgette", "Tobias", "Leona", "Franklin", "Annie", "Blake", "Beth", "Barnaby", "Brie", "Elijah", "Aubrey"]
     return(random.choice(name_list))
     
 def choose_random_job():
